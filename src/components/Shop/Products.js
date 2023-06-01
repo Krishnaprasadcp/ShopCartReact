2a325c132359f6e73290b5f084a0e4fd0419f6ca
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-06-01 08:36:49 -0700

    Commit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const Dummy_Products = [{
+  id:'p1',
+  title:'Test',
+  price:6,
+  description:'This is a first product - amazing!'
+},
+{
+  id:'p2',
+  title:'Book',
+  price:4,
+  description:'This is a second product - amazing!'
+}
+];
+const Products = (props) => {
+
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {Dummy_Products.map((products)=>(
+          <ProductItem
+            key={products.id}
+            id={products.id}
+            title={products.title}
+            price={products.price}
+            description={products.description}
+          />
+        ))
+          }
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
